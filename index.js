ceb35d17f4e059b6fb4efd8fc56bde9834703968
Elio <netoo.elio@hotmail.com>
2022-11-01 16:53:36 -0300

    obtendo dados via cheerio

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const rp = require("request-promise");
+const cheerio = require("cheerio");
 
-const url = "https://pt.wikipedia.org/wiki/Lista_de_presidentes_do_Brasil";
+const url =
+  "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
 
 rp(url)
   .then((html) => {
-    console.log(html);
+    //console.log(html);
+    const $ = cheerio.load(html); // Carrega o HTML para a constante $
+    console.log($("td > b > a", html).length); // <td><b><a></a></b></td>
+    console.log($("td > b > a", html)); // imprime os nodes no html
   })
   .catch((err) => {
     console.error(err);
